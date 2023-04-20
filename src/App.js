5787ff241851be5f567f2dd99d0e5dca05742001
Janvierjavi <janvierjavim2@gmail.com>
2023-04-20 17:28:32 +0200

    added paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgetPassword from "./pages/ForgetPassword";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
-  return <h1>hello world!!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="/Sign-Up" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
