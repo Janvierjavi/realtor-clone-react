c92ccd5fab92859c3049e5605ad474545c6b7c85
Janvierjavi <janvierjavim2@gmail.com>
2023-06-03 16:10:39 +0200

    add my listing for the profile  and create components

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -77,7 +77,7 @@ export default function CreateListing() {
   async function onSubmit(e) {
     e.preventDefault();
     setLoading(true);
-    if (discountedPrice <= regularPrice) {
+    if (discountedPrice >= regularPrice) {
       setLoading(false);
       toast.error("Discounted price needs to be less than regular price");
       return;
@@ -158,6 +158,7 @@ export default function CreateListing() {
       imgUrls,
       geolocation,
       timestamp: serverTimestamp(),
+      userRef: auth.currentUser.uid,
     };
     delete formDataCopy.images;
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
